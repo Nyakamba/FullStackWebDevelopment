a4164bbb8629a1c4e8980c7765040add0730ac0f
Nyakamba <omwegaenock@gmail.com>
2024-01-12 20:27:12 +0300

    arithmetic operations

--- 5.Arithmetyic-Operations/script.js
+++ 5.Arithmetyic-Operations/script.js
@@ -4,10 +4,24 @@ let a = 5;
 let b = 2;
 
 // Addition
-
 // const results = a + b;
 
 // Subtraction
+// const results = a - b;
 
-const results = a - b;
-console.log(results);
+// multiplication
+// const results = a * b;
+
+// division
+// const results = a / b;
+
+// Using Modulus
+// const results = a % b;
+
+// Increament nd Decrement
+let number = 10;
+// number++; increment
+
+number--; //decrement
+
+console.log(number);
