396005528cb5f90c2142af0694c14612a4260b82
Nyakamba <omwegaenock@gmail.com>
2024-01-12 20:16:34 +0300

    variables

---
+++ 2.Variables/script.js
@@ -0,0 +1,19 @@
+// !-----using var----
+// Task storing a person's name
+
+// var firstName = "Enock";
+// console.log(firstName);
+
+// !-----using let----
+
+// Task storing a person's name
+
+// let firstName = "Enock";
+// console.log(firstName);
+
+// !-----using const----
+
+// Task storing a person's name
+
+// const firstName = "Enock";
+// console.log(firstName);
