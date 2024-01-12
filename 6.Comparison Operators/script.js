cf139081034ceda3d98a2cde109c1c4555f271a0
Nyakamba <omwegaenock@gmail.com>
2024-01-12 20:36:55 +0300

    copmarison operators

---
+++ 6.Comparison Operators/script.js
@@ -0,0 +1,10 @@
+// Define the variables
+
+let ageOfJohn = 25; //Number
+let ageOfDoe = 30;
+let userInput = "25"; //String
+//using == (double quote)
+// console.log(ageOfJohn == userInput);
+
+//using ===
+console.log(ageOfJohn === userInput);
