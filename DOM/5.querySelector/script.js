89971fe0709872672a248e550c9106a27515d846
Nyakamba <omwegaenock@gmail.com>
2024-01-16 17:57:33 +0300

    querySelector

---
+++ DOM/5.querySelector/script.js
@@ -0,0 +1,20 @@
+// * Get the first product card
+
+const firstProductCard = document.querySelector(".product-card");
+
+//console.log(firstProductCard);
+
+// *Get the highest-rated product
+
+const highestRatingEl = document.querySelector("[data-rating='4.8']");
+//console.log(highestRatingEl);
+
+// *Get the first laptop category product
+
+const laptopElement = document.querySelector("[data-category='laptop']");
+//console.log(laptopElement);
+
+//Selecting an id
+
+const dashboardEl = document.querySelector("#dashboard");
+console.log(dashboardEl);
