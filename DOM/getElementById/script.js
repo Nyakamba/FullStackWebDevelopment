b93b0bab0929ab359871ecf3553d88db35031681
Nyakamba <omwegaenock@gmail.com>
2024-01-16 17:05:42 +0300

    getElementById

---
+++ DOM/getElementById/script.js
@@ -0,0 +1,12 @@
+// *-------
+// * Selcting elements using getElementById
+// *-------
+
+const conferenceTitleEl = document.getElementById("conferenceTitle");
+const conferenceDateEl = document.getElementById("conferenceDate");
+const locationTitleEl = document.getElementById("locationTitle");
+const conferenceLocationEl = document.getElementById("conferenceLocation");
+const speakersTitleEl = document.getElementById("speakersTitle");
+const speakerListEl = document.getElementById("speakerList");
+
+console.log(conferenceDateEl, locationTitleEl, conferenceLocationEl);
