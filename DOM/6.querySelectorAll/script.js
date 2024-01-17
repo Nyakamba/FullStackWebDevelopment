caf25d19c2195a4e3953f68a0ede50b1317dec4f
Nyakamba <omwegaenock@gmail.com>
2024-01-17 11:16:19 +0300

    querySelectorAll

--- DOM/6.querySelectorAll/script.js
+++ DOM/6.querySelectorAll/script.js
@@ -6,7 +6,26 @@ const productCartEls = document.querySelectorAll(".product-card");
 const productNameEls = document.querySelectorAll(".product-name");
 
 productNameEls.forEach((element) => {
-  console.log(element.innerHTML);
+  // console.log(element.innerHTML);
 });
 
 //select and log  all prices of laptop category products
+
+const laptopPricesEls = document.querySelectorAll(
+  "[data-category='laptop'] .product-price"
+);
+
+//console.log(laptopPricesEls);
+
+laptopPricesEls.forEach((el) => {
+  console.log(el.textContent);
+});
+
+//Select all products  with a rating above 4.5
+const highestRatingsEl = document.querySelectorAll(
+  '[data-rating="4.7"], [data-rating="4.8"]'
+);
+
+highestRatingsEl.forEach((el) => {
+  console.log(el.innerHTML);
+});
