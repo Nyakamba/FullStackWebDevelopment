7cc7d98c3aa4fd0a50bf1d56fd7a73d8d41125fa
Nyakamba <omwegaenock@gmail.com>
2024-01-18 19:30:19 +0300

    blocking vs non-blocking code

---
+++ Asynchronous Programming/2.Blocking vs non-blocking code/script.js
@@ -0,0 +1,19 @@
+//---Create a blocking ----
+
+// console.log("Starting Blocking Operations");
+
+// //-----Create blocking code
+// //for (let i = 0; i < 1e9; i++) {
+// //do somthing
+// //console.log(i);
+
+// console.log("Finished Blocking Operations");
+
+//--Create a non-blocking---
+
+console.log("starting non blocking operation");
+
+setTimeout(() => {
+  console.log("Executing delay");
+}, 4000);
+console.log("Finshed non bocking operation");
