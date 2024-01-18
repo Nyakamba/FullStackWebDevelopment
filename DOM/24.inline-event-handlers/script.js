11328156a972cbb9ffa94d231397941ddcf99d0d
Nyakamba <omwegaenock@gmail.com>
2024-01-18 14:54:13 +0300

    inline-event-handlers

---
+++ DOM/24.inline-event-handlers/script.js
@@ -0,0 +1,48 @@
+//---Mouse event-----
+
+function clickHandler() {
+  //console.log("CLick event is called");
+}
+
+function dbClickHandler() {
+  //console.log("DoubleCLick event is called");
+}
+
+function onMouseOverHandler() {
+  //console.log("onMouseOverHandler event is called");
+}
+
+function onMouseOutHandler() {
+  //console.log("onMouseOutHandler event is called");
+}
+
+//-- Keyboard Events ----
+
+function onkeydownHandler() {
+  //console.log("onkeydownHandler event is called");
+}
+function onkeyupHandler() {
+  //console.log("onkeyupHandler event is called");
+}
+
+function onkeypressHandler() {
+  //console.log("onkeypressHandler event is called");
+}
+
+function onkeypressHandler() {
+  //console.log("onkeypressHandler event is called");
+}
+
+// -- Form Events --
+
+function onsubmitHandler() {
+  //console.log("onsubmitHandler event is called");
+}
+
+function onfocusHandler() {
+  console.log("onfocusHandler event is called");
+}
+
+function onblurHandler() {
+  console.log("onblurHandler event is called");
+}
