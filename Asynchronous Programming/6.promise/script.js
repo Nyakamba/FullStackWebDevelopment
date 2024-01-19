fd752bff6a8f2387f0d7b3613f85796d8e9caf77
Nyakamba <omwegaenock@gmail.com>
2024-01-19 15:05:02 +0300

    promise chaining

--- Asynchronous Programming/6.promise/script.js
+++ Asynchronous Programming/6.promise/script.js
@@ -1,95 +1,37 @@
-//*Example 1 with callback
+//* First function returning a promise
 
-//creating callback
-// function simpleCallback1(success, cb) {
-//   console.log("Cd function called");
-//   if (success) {
-//     cb(null, "The operation was successiful");
-//   } else {
-//     cb("the operation failed", null);
-//   }
-// }
-
-// //usage callback function
-
-// simpleCallback1(false, (err, result) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log(result);
-//   }
-//   console.log("This always runs");
-// });
-
-//* Example 1 convert to use promise
-
-//creation of promise
-
-// const simplePromise = new Promise((resolve, rejected) => {
-//   const success = false;
-//   if (success) {
-//     //resolve the promise
-//     resolve("The promise operation was successiful");
-//   } else {
-//     //reject the operation
-//     rejected("The promise operation rejected");
-//   }
-// });
-
-// //usage
-
-// simplePromise
-//   .then((result) => {
-//     console.log(result);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   })
-//   .finally(() => {
-//     console.log("Always runs");
-//   });
-
-//* Example 2 using callback promise
-
-// function isEvenNumber(number, cb) {
-//   if (number % 2 === 0) {
-//     cb(null, `The number ${number} is even`);
-//   } else {
-//     cb(new Error(`The number ${number} is not even`));
-//   }
-// }
-
-// //usage
-
-// isEvenNumber(5, (err, result) => {
-//   if (err) {
-//     console.log(err.message);
-//   } else {
-//     console.log(result);
-//   }
-// });
-
-//* Example 2 convert to use promise
-
-//creation of promise
-
-function isEvenNumberPomise(number) {
-  return new Promise((resolve, reject) => {
-    if (number % 2 === 0) {
-      //resolve
-      resolve(`The number ${number} is even`);
-    } else {
-      //reject
-      reject(`The number ${number} is not even`);
-    }
+function stepOnePromise() {
+  return new Promise((reolve, reject) => {
+    reolve("Step 1 completed");
   });
 }
 
-//call the fn/rsolve
+function stepOTwoPromise() {
+  return new Promise((reolve, reject) => {
+    reolve("Step 2 completed");
+  });
+}
 
-isEvenNumberPomise(9)
-  .then((result) => {
-    console.log(result);
+function stepThreeromise() {
+  return new Promise((reolve, reject) => {
+    reolve("Step 3 completed");
+  });
+}
+
+//chaining
+stepOnePromise()
+  .then((resultFromP1) => {
+    console.log(resultFromP1);
+    //second promise
+    return stepOTwoPromise();
+  })
+  .then((resultFromP2) => {
+    console.log(resultFromP2);
+    //thrid promise
+    return stepThreeromise();
+  })
+  .then((resultFromP3) => {
+    console.log(resultFromP3);
   })
   .catch((err) => {
     console.log(err);
