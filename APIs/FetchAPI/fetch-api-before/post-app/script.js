11b893f4c662b718ce9b7a097aeba7e4e397c800
Nyakamba <omwegaenock@gmail.com>
2024-01-20 15:06:35 +0300

    fetchApi

--- APIs/FetchAPI/fetch-api-before/post-app/script.js
+++ APIs/FetchAPI/fetch-api-before/post-app/script.js
@@ -3,3 +3,42 @@ const title = document.querySelector(".title");
 const btn = document.querySelector("#btn");
 
 //Making http request
+
+//Get the endpoint of the serve/api
+const url = "https://jsonplaceholder.typicode.com/posts";
+
+// fetch(url)
+//   .then((res) => {
+//     res.json().then((data) => {
+//       console.log(data);
+//     });
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+const fetchPosts = async () => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    //display data into the DOM
+    const result = data.map((post) => {
+      return `<div class="card">
+        <div class="card-title">
+          <h2>${post.title}</h2>
+        </div>
+        <div class="card-body">
+          <p>
+            ${post.body}
+          </p>
+        </div>
+      </div>`;
+    });
+
+    container.innerHTML = result.join("");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+btn.addEventListener("click", fetchPosts);
