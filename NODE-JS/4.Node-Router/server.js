04781996ed5cc66cf1673b3ebadf418fc8fbf6db
Nyakamba <omwegaenock@gmail.com>
2024-01-21 18:22:26 +0300

    node routes

--- NODE-JS/4.Node-Router/server.js
+++ NODE-JS/4.Node-Router/server.js
@@ -1,11 +1,27 @@
-//* import required modules
 const http = require("http");
+const url = require("url");
+
+//*Routes
 // console.log(http);
 //*Define the handler
 const requestHandler = (req, res) => {
-  //send response
-  res.writeHead(200, { "content-type": "text/plain" });
-  res.end("Hello world. this is my first Node js server");
+  //pass the url
+  const passedUrl = url.parse(req.url, true);
+  //Home route
+  if (passedUrl.pathname === "/" && req.method === "GET") {
+    //send response
+    res.writeHead(200, { "content-type": "text/plain" });
+    res.end("Hello world. welcome to homepage");
+  }
+  //about route
+  else if (passedUrl.pathname === "/about" && req.method === "GET") {
+    //send response
+    res.writeHead(200, { "content-type": "text/plain" });
+    res.end("Hello world. welcome to about page");
+  } else {
+    res.writeHead(200, { "content-type": "text/plain" });
+    res.end("Not Found");
+  }
 };
 //*Create the server
 const server = http.createServer(requestHandler);
