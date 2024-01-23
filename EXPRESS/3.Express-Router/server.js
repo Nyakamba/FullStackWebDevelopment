763a24e8ff90355e63ddd00d195db9469fd8757e
Nyakamba <omwegaenock@gmail.com>
2024-01-23 10:23:03 +0300

    express router

--- EXPRESS/3.Express-Router/server.js
+++ EXPRESS/3.Express-Router/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const userRouter = require("./routes/usersRouter");
+const postRouter = require("./routes/postsRouter");
 const app = express();
 
 const PORT = 8082;
@@ -10,5 +11,8 @@ app.get("/", (req, res) => {
 });
 // *----USERS ROUTE----
 app.use("/users", userRouter);
+//*-----POSTS ROUTE----
+
+app.use("/posts", postRouter);
 //satrt server
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
