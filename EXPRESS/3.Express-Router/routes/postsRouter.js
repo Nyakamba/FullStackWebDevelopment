5e1641809ba41c8d62c5915bc7930e648801a386
Nyakamba <omwegaenock@gmail.com>
2024-01-23 10:23:14 +0300

    Express router

---
+++ EXPRESS/3.Express-Router/routes/postsRouter.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const postRouter = express.Router();
+
+postRouter.get("/", (req, res) => {
+  res.json({ message: "All posts" });
+});
+
+module.exports = postRouter;
