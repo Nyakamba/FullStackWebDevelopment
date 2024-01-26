6d399eb6b892c76fb7916f4e674d2f170b8f789c
Nyakamba <omwegaenock@gmail.com>
2024-01-26 10:50:35 +0300

    gettoken from headerrs

---
+++ Authntication&Authorization/4.JWT-Authentication/utils/getTokenFromHeaders.js
@@ -0,0 +1,12 @@
+const getTokenFromHeaders = (req) => {
+  const headerObj = req.headers;
+  const token = headerObj["authorization"].split(" ")[1];
+  if (token !== undefined) {
+    return token;
+  }
+  return {
+    status: "Failed",
+    message: "There is no token attached to the header",
+  };
+};
+module.exports = getTokenFromHeaders;
