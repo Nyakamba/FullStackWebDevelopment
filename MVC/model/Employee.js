e475f8aac8b3998aefff09c19955497f0b54396d
Nyakamba <omwegaenock@gmail.com>
2024-01-26 14:00:02 +0300

    employee model

--- MVC/model/Employee.js
+++ MVC/model/Employee.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
 
 //schema
-const employeeSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  city: { type: String, required: true },
-  isMarried: { type: Boolean, required: true },
-  salary: { type: Number, required: true },
-});
+const employeeSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    city: { type: String, required: true },
+    isMarried: { type: Boolean, required: true },
+    salary: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
+
+const Employee = mongoose.model("Employee", employeeSchema);
+module.exports = Employee;
