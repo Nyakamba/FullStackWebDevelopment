a7560b0e41ebeb2b0f78e2bf9803fc1bbe060366
Nyakamba <omwegaenock@gmail.com>
2024-01-26 14:53:13 +0300

    MVC routing

--- MVC/server.js
+++ MVC/server.js
@@ -1,70 +1,13 @@
 require("./utils/dbConnect"); //database connection
 const express = require("express");
+const router = require("./routes/router");
 const app = express();
 
 //middlewares
 app.use(express.json()); //pass incoming data
 
 //routes
-//POST/employess
-app.post("/employees", async (req, res) => {
-  try {
-    res.json({
-      status: "Success",
-      data: "Create Employee",
-    });
-  } catch (error) {
-    res.json(error.message);
-  }
-});
-
-//GET/employess
-app.get("/employees", async (req, res) => {
-  try {
-    res.json({
-      status: "Success",
-      data: "Get Employees",
-    });
-  } catch (error) {
-    res.json(error.message);
-  }
-});
-
-//PUT/employess/:id
-app.put("/employees/:id", async (req, res) => {
-  try {
-    res.json({
-      status: "Success",
-      data: "Update Employee",
-    });
-  } catch (error) {
-    res.json(error.message);
-  }
-});
-
-//Delete/employess/:id
-app.delete("/employees/:id", async (req, res) => {
-  try {
-    res.json({
-      status: "Success",
-      data: "Delete Employee",
-    });
-  } catch (error) {
-    res.json(error.message);
-  }
-});
-
-//GET/employess
-app.get("/employees/:id", async (req, res) => {
-  try {
-    res.json({
-      status: "Success",
-      data: "fetch a single  Employee",
-    });
-  } catch (error) {
-    res.json(error.message);
-  }
-});
+app.use("/employees", router);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
