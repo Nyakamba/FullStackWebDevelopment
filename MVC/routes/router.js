2bd28808e4ac191f8a2b1eff2e358ee64af83840
Nyakamba <omwegaenock@gmail.com>
2024-01-26 20:39:05 +0300

    controllers

--- MVC/routes/router.js
+++ MVC/routes/router.js
@@ -1,65 +1,26 @@
 const express = require("express");
-
+const {
+  createEmployeeCtrl,
+  getEmployeesCtrl,
+  updateEmployeeCtrl,
+  deleteEmployeeCtrl,
+  getEmployeeCtrl,
+} = require("../controller/employeeCtrl");
 const router = express.Router();
 
-//POST/employess
-router.post("/", async (req, res) => {
-  try {
-    res.json({
-      status: "Success",
-      data: "Create Employee",
-    });
-  } catch (error) {
-    res.json(error.message);
-  }
-});
+//POST/employee
+router.post("/", createEmployeeCtrl);
 
-//GET/employess
-router.get("/", async (req, res) => {
-  try {
-    res.json({
-      status: "Success",
-      data: "Get Employees",
-    });
-  } catch (error) {
-    res.json(error.message);
-  }
-});
+//GET/employees
+router.get("/", getEmployeesCtrl);
 
-//PUT/employess/:id
-router.put("/:id", async (req, res) => {
-  try {
-    res.json({
-      status: "Success",
-      data: "Update Employee",
-    });
-  } catch (error) {
-    res.json(error.message);
-  }
-});
+//PUT/employee/:id
+router.put("/:id", updateEmployeeCtrl);
 
-//Delete/employess/:id
-router.delete("/:id", async (req, res) => {
-  try {
-    res.json({
-      status: "Success",
-      data: "Delete Employee",
-    });
-  } catch (error) {
-    res.json(error.message);
-  }
-});
+//Delete/employees/:id
+router.delete("/:id", deleteEmployeeCtrl);
 
-//GET/employess
-router.get("/:id", async (req, res) => {
-  try {
-    res.json({
-      status: "Success",
-      data: "fetch a single  Employee",
-    });
-  } catch (error) {
-    res.json(error.message);
-  }
-});
+//GET/employee
+router.get("/:id", getEmployeeCtrl);
 
 module.exports = router;
