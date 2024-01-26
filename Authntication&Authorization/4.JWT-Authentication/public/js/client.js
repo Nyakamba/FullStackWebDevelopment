ecc7d9bd4b24facc03c289a649d14cb350697d4e
Nyakamba <omwegaenock@gmail.com>
2024-01-26 13:15:10 +0300

    storing token into local storage

---
+++ Authntication&Authorization/4.JWT-Authentication/public/js/client.js
@@ -0,0 +1,19 @@
+const form = document.querySelector("form");
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
+
+form.addEventListener("submit", async (e) => {
+  //prevent default browser
+  e.preventDefault();
+
+  const response = await axios.post("/login", {
+    username: username.value,
+    password: password.value,
+  });
+  if (response.data.status === "Success") {
+    //save the token into local storage
+    localStorage.setItem("token", response.data.token);
+  } else {
+    alert("Sorry try again");
+  }
+});
